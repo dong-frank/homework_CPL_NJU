eebd9ffdaf19a86857da508fdde4c65669c78003
Frank <dongshen_rao@smial.nju.edu.cn>
2023-09-23 11:16:57 +0800

    finished Equation

---
+++ 1-types-io/Equation.c
@@ -0,0 +1,23 @@
+//
+// Created by Frank on 2023/9/23.
+//
+#include <stdio.h>
+#include <math.h>
+
+int main (void){
+    int p;
+    int q;
+    double first_number;
+    double second_number;
+    double x_1;
+
+    scanf("%d%d",&p,&q);
+
+    first_number= cbrt(- (q/2.0) + sqrt((q/2.0) * (q/2.0) + (p/3.0) * (p/3.0) * (p/3.0)));
+    second_number= cbrt(- (q/2.0) - sqrt((q/2.0) * (q/2.0) + (p/3.0) * (p/3.0) * (p/3.0)));
+    x_1= first_number + second_number;
+
+    printf("%.3f",x_1);
+
+    return 0;
+}
