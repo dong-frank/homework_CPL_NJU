dc4da715840d20a5dc22c88a55d2aff771cd945a
Frank <dongshen_rao@smial.nju.edu.cn>
2023-10-12 19:18:09 +0800

    correct factorial.c

--- 2-if-for-array/factorial.c
+++ 2-if-for-array/factorial.c
@@ -5,20 +5,20 @@
 
 int main(void) {
     int n;
-    const int m =10007;
+    const int m = 10007;
     scanf("%d", &n);
-    int factorial=1;
-    int sum_of_factorial=0;
-    for (int i = 1; i <=n ; i++) {
-        for (int j = 1; j <=i; j++) {
-            factorial*=(j%m);
-            factorial%=m;
+    int factorial = 1;
+    int sum_of_factorial = 0;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            factorial *= j;
+            factorial %= m;
         }
 
         sum_of_factorial += factorial;
-        factorial=1;
+        factorial = 1;
     }
-    sum_of_factorial%=m;
+    sum_of_factorial %= m;
     printf("%d", sum_of_factorial);
     return 0;
 }
